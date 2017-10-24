539533a204acf2f9dca25c3d18faa2d55d73cd6d
Ryan <ryanjones@Ryans-MacBook-Pro.local>
2017-10-24 11:26:22 +0200

    lazy loading for profile module

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './template/home-template-wrapper/home.component';
-import { MyAboutComponent } from './my-about/my-about.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { NotFoundComponent } from 'app/not-found/not-found.component';
 import { MyFlightsComponent } from 'app/flight-checker/my-flights/my-flights.component';
+import { MySignupFormComponent } from 'app/auth/my-signup-form/my-signup-form.component';
+import { MyLoginComponent } from 'app/auth/my-login/my-login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: MyAboutComponent },
+  { path: 'itinerary', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
   { path: 'flights', component: MyFlightsComponent },
-  
+  { path: 'signup', component: MySignupFormComponent },
+  { path: 'login', component: MyLoginComponent },
+  {
+    path: 'profile',
+    loadChildren: './profiles/profiles.module#ProfilesModule'
+  },
   { path: 'not-found', component: NotFoundComponent },
-  { path: '**', redirectTo: '/not-found'}
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class RoutingModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
-import { AuthRoutes } from "app/auth/auth.routes";
 
 import { MyLoginComponent } from 'app/auth/my-login/my-login.component';
 import { MySignupFormComponent } from 'app/auth/my-signup-form/my-signup-form.component';
 
-
 @NgModule({
   declarations: [MyLoginComponent, MySignupFormComponent],
-  imports: [AuthRoutes, SharedModule],
+  imports: [SharedModule],
+  exports: [MyLoginComponent, MySignupFormComponent]
 })
 export class AuthModule {}
